7e7091b3fbcdd3eeec4fbb41ec7c49593a4bf0c1
jupitvq <40255470+jupitvq@users.noreply.github.com>
2024-11-07 16:39:07 +0700

    🪛 Penambahan sub-intents, optimalisasi parser, dan fix modelling dengan sub-intents

--- util/parser.py
+++ util/parser.py
@@ -14,20 +14,32 @@ class JSONParser:
             self.data = json.load(data_file)
 
         for intent in self.data['intents']:
-            for pattern in intent['patterns']:
-                self.text.append(pattern)
-                self.intents.append(intent['tag'])
-            for resp in intent['responses']:
-                if intent['tag'] in self.responses.keys():
-                    self.responses[intent['tag']].append(resp)
-                else:
-                    self.responses[intent['tag']] = [resp]
+            # Check if subintents exist
+            if 'subintents' in intent:
+                for subintent in intent['subintents']:
+                    for pattern in subintent['patterns']:
+                        self.text.append(pattern)
+                        self.intents.append(subintent['tag'])
+                    for resp in subintent['responses']:
+                        if subintent['tag'] in self.responses:
+                            self.responses[subintent['tag']].append(resp)
+                        else:
+                            self.responses[subintent['tag']] = [resp]
+            else:
+                # Original intent parsing if no subintents
+                for pattern in intent['patterns']:
+                    self.text.append(pattern)
+                    self.intents.append(intent['tag'])
+                for resp in intent['responses']:
+                    if intent['tag'] in self.responses:
+                        self.responses[intent['tag']].append(resp)
+                    else:
+                        self.responses[intent['tag']] = [resp]
 
-        self.df = pd.DataFrame({'text_input': self.text,
-                                'intents': self.intents})
+        # Create DataFrame with updated intents and patterns
+        self.df = pd.DataFrame({'text_input': self.text, 'intents': self.intents})
 
-        print(
-            f"[INFO] Data JSON converted to DataFrame with shape : {self.df.shape}")
+        print(f"[INFO] Data JSON converted to DataFrame with shape : {self.df.shape}")
 
     def get_dataframe(self):
         return self.df
