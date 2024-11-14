fda03c124c7a8d25915cf94f5c27e3fa149a4fd4
jupitvq <40255470+jupitvq@users.noreply.github.com>
2024-11-14 21:45:00 +0700

    🆕 Enhance Telegram bot responses and add 'bantu' command for question recommendations

--- telegram_bot.py
+++ telegram_bot.py
@@ -22,7 +22,7 @@ def bot_response(chat, pipeline, jp):
     res = pipeline.predict_proba([chat])
     max_prob = max(res[0])
     if max_prob < 0.065:
-        return "🙏 *Maaf, saya tidak dapat memberikan jawaban untuk pertanyaan tersebut.*\n\nNamun, jika Anda membutuhkan informasi lebih lanjut atau memiliki pertanyaan yang lebih spesifik, silahkan coba ketik ulang dengan lebih detail atau menghubungi *Humas UIB*.\n\nTim Humas kami siap memberikan penjelasan yang lebih mendalam dan menjawab pertanyaan Anda yang lebih detail.", None
+        return "🙏 *Maaf, saya tidak dapat memberikan jawaban untuk pertanyaan tersebut.*\n\nNamun, jika Anda membutuhkan informasi lebih lanjut atau memiliki pertanyaan yang lebih spesifik, silahkan coba ketik ulang dengan lebih detail atau *menghubungi Humas UIB*.\n\nTim Humas kami siap memberikan penjelasan yang lebih mendalam dan menjawab pertanyaan Anda yang lebih detail.", None
     else:
         max_id = np.argmax(res[0])
         pred_tag = pipeline.classes_[max_id]
@@ -40,7 +40,6 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-# Function to get a random pattern from subintents
 def get_random_pattern(jp):
     patterns = []
     for intent in jp.data['intents']:
@@ -49,18 +48,21 @@ def get_random_pattern(jp):
                 patterns.extend(subintent['patterns'])
     return random.choice(patterns)
 
-# Function to create inline keyboard
 def create_inline_keyboard(pattern):
     keyboard = [
-        [InlineKeyboardButton("Tanya", callback_data=pattern[:64]), InlineKeyboardButton("Minta Pertanyaan Lain", callback_data="minta_pertanyaan_lain")]
+        [InlineKeyboardButton("Tanya", callback_data=pattern[:64]), InlineKeyboardButton("Pertanyaan Lain", callback_data="minta_pertanyaan_lain")]
     ]
     return InlineKeyboardMarkup(keyboard)
 
+def create_inline_keyboard_with_3_buttons(patterns):
+    keyboard = [[InlineKeyboardButton(pattern, callback_data=pattern[:64])] for pattern in patterns]
+    return InlineKeyboardMarkup(keyboard)
+
 async def start(update: Update, context: CallbackContext) -> None:
-    await update.message.reply_text('*Hi!* Anda sedang terhubung dengan chatbot kami yang saat ini masih dalam tahap pengembangan. 😊\n\nKami sedang bekerja keras untuk meningkatkan kemampuan chatbot ini agar dapat memberikan informasi yang lebih lengkap dan akurat. Saat ini, chatbot ini bisa memberikan jawaban terbatas, namun tim kami selalu siap membantu jika Anda membutuhkan informasi lebih lanjut.\n\nTerima kasih atas kesabaran Anda, dan kami akan terus berusaha memberikan pengalaman yang lebih baik!', parse_mode='Markdown')
+    await update.message.reply_text('*Hi!* Anda sedang terhubung dengan chatbot kami yang saat ini masih dalam tahap pengembangan. 😊\n\nKami sedang bekerja keras untuk meningkatkan kemampuan chatbot ini agar dapat memberikan informasi yang lebih lengkap dan akurat. Saat ini, chatbot ini bisa memberikan jawaban terbatas, namun tim kami selalu siap membantu jika Anda membutuhkan informasi lebih lanjut.\n\nTerima kasih atas kesabaran Anda, dan kami akan terus berusaha memberikan pengalaman yang lebih baik! 💪\n\n*Semoga hari Anda menyenangkan!* 😊', parse_mode='Markdown')
 
 async def help_command(update: Update, context: CallbackContext) -> None:
-    await update.message.reply_text("*Untuk memulai percakapan, ketik /start.*\n\nSetelah Anda siap, ketik perintah tersebut untuk mengakses layanan kami.\n\n*Untuk rekomendasi pertanyaan, ketik /tanya.* Gunakan perintah ini untuk mendapatkan rekomendasi pertanyaan dari chatbot kami.\n\n*Untuk mengakhiri chat, ketik 'bye'.* Jika Anda sudah selesai atau ingin berhenti, cukup ketik 'bye' dan percakapan akan berakhir. Kami siap membantu kapan saja!", parse_mode='Markdown')
+    await update.message.reply_text("*Untuk memulai percakapan, ketik /start.*\n\nSetelah Anda siap, ketik perintah tersebut untuk mengakses layanan kami.\n\n*Untuk rekomendasi pertanyaan, ketik /tanya.* Gunakan perintah ini untuk mendapatkan rekomendasi pertanyaan dari chatbot kami.\n\n*Untuk mengakhiri chat, ketik 'bye'.* Jika Anda sudah selesai atau ingin berhenti, cukup ketik 'bye' dan percakapan akan berakhir. Kami siap membantu kapan saja!\n\n*Untuk rekomendasi pertanyaan yang menarik, ketik /bantu.* Gunakan perintah ini untuk mendapatkan ide pertanyaan yang bisa Anda tanyakan kepada chatbot kami.\n\n*Semoga informasi ini membantu!* 😊", parse_mode='Markdown')
 
 async def handle_message(update: Update, context: CallbackContext) -> None:
     user_message = update.message.text
@@ -74,6 +76,16 @@ async def random_pattern_command(update: Update, context: CallbackContext) -> No
     keyboard = create_inline_keyboard(pattern)
     await update.message.reply_text(f"✨ *Rekomendasi Pertanyaan* ✨\nBerikut adalah pertanyaan yang mungkin membantu Anda:\n\n**{pattern}**", reply_markup=keyboard, parse_mode='Markdown')
 
+async def bantu_command(update: Update, context: CallbackContext) -> None:
+    patterns = []
+    for intent in jp.data['intents']:
+        if 'subintents' in intent:
+            for subintent in intent['subintents']:
+                patterns.extend(subintent['patterns'])
+    random_patterns = random.sample(patterns, 3)
+    keyboard = create_inline_keyboard_with_3_buttons(random_patterns)
+    await update.message.reply_text("✨ *Daftar Pertanyaan* ✨\nBerikut adalah beberapa pertanyaan yang mungkin membantu Anda:", reply_markup=keyboard, parse_mode='Markdown')
+
 async def button(update: Update, context: CallbackContext) -> None:
     query = update.callback_query
     await query.answer()
@@ -82,10 +94,10 @@ async def button(update: Update, context: CallbackContext) -> None:
     if user_message == "minta_pertanyaan_lain":
         pattern = get_random_pattern(jp)
         keyboard = create_inline_keyboard(pattern)
-        await query.edit_message_text(f"✨ *Rekomendasi Pertanyaan* ✨\nBerikut adalah pertanyaan baru yang mungkin lebih bagus dari sebelumnya:\n\n**{pattern}**", reply_markup=keyboard, parse_mode='Markdown')
+        await query.edit_message_text(f"✨ *Rekomendasi Pertanyaan* ✨\nBerikut adalah pertanyaan yang mungkin membantu Anda:\n\n**{pattern}**", reply_markup=keyboard, parse_mode='Markdown')
     else:
         response, tag = bot_response(user_message, pipeline, jp)
-        await query.edit_message_text(f"Anda: {user_message}", parse_mode='Markdown')
+        await query.edit_message_text(f"*👤 Anda:* {user_message}", parse_mode='Markdown')
         await query.message.reply_text(response, parse_mode='Markdown')
 
 def main() -> None:
@@ -98,6 +110,7 @@ def main() -> None:
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("help", help_command))
     application.add_handler(CommandHandler("tanya", random_pattern_command))
+    application.add_handler(CommandHandler("bantu", bantu_command))
     application.add_handler(CallbackQueryHandler(button))
 
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
